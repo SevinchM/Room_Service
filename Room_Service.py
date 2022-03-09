bf15b70ca4a61a882aa776e232b8b6afb78d510f
SevinchM <sevinchbegim.m@asu.edu.mn>
2022-03-09 13:57:20 +0800

    Created a dictionary

--- Room_Service.py
+++ Room_Service.py
@@ -1,1 +1,14 @@
-#Hola holita
+#A Room Service Chatbot to order food to a hotel Room.
+
+import datetime
+
+guests = {'01':'Mr.V','02':'Joy','03':'Khaliun','04':'Jenny','05':'Ulemj', '06':'Connie', '07':'Dari'}
+appetizers = {'101': 'Ceasar Salad', '102': 'Meat Platter', '103': 'Mini Quiche'}
+main_menu = {'201':'Filet Mignon', '202':'Chicken Quesadilla','203':'Mushroom Cream Soup'}
+dessserts = {'301': 'Ice Cream', '302':'Cheesecake', '303':'Fruit Platter'}
+beverages = {'A': 'Tea', 'B':'Coffee', 'C':'Juice'}
+
+name = None
+room = None
+
+order = {}
